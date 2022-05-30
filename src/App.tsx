efc0fce9cb5c05aaf85cbc24ec0b8c59ed55fcd5
theborakompanioni <theborakompanioni+github@gmail.com>
2022-05-30 22:05:26 +0200

    dev: show fen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,61 @@
 
-import React from 'react';
-import Chessground from '@react-chess/chessground';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Chessground } from 'chessground';
+import { Api as ChessgroundApi } from 'chessground/api';
+import 'chessground/assets/chessground.base.css';
+import 'chessground/assets/chessground.brown.css';
+import 'chessground/assets/chessground.cburnett.css';
+
 import './App.css';
 
 function App() {
+  const groundRef = useRef<HTMLDivElement>(null)
+  const [ground, setGround] = useState<ChessgroundApi | null>(null)
+  const [changeCounter, setChangeCounter] = useState(0)
+  const increaseChangeCounter = useCallback(() => setChangeCounter((current) => current + 1), [])
+
+  const fen = useMemo(() => changeCounter >= 0 && ground?.getFen(), [ground, changeCounter])
+
+  const onChange = useCallback(() => {
+    increaseChangeCounter()
+  }, [increaseChangeCounter])
+
+  const config = useMemo(() => ({
+    movable: {
+      free: true
+    },
+    draggable: {
+      enabled: true,
+      deleteOnDropOff: true,
+    },
+    drawable: {
+      enabled: true
+    },
+    events: {
+      change: onChange
+    }
+  }), [onChange])
+
+  useEffect(() => {
+    if (!groundRef || !groundRef.current) return
+
+    const ground = Chessground(groundRef.current, config);
+    setGround(ground)
+
+    return () => {
+      ground.destroy()
+    }
+  }, [groundRef, config])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {fen}
         </p>
         <div style={{ height: '400px', width: '400px' }}>
-          <Chessground contained={true} config={{
+          <div ref={groundRef} style={{ height: '400px', width: '400px' }}></div>
+          {/*<ChessgroundReact contained={true} config={{
             movable: {
               free: true
             },
@@ -21,7 +65,7 @@ function App() {
             },
             drawable: {
               enabled: true
-            }}}/>
+            }}}/>*/}
         </div>
       </header>
     </div>
